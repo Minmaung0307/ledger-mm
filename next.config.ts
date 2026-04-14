7184d11f2056f8c2db01577818efde6bb7506bce
Min Maung <minmaung0307@gmail.com>
2026-04-14 12:01:41 -0400

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups', // ဒါလေးက Pop-up ပိတ်တာကို ခွင့်ပြုပေးမှာပါ
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
